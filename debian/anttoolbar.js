10c8c047020d29722c93932d1c178be3c8810124
Rohan Garg <rohan@garg.io>
2016-12-16 16:00:05 +0100

    Add preferences

---
+++ debian/anttoolbar.js
@@ -0,0 +1,6 @@
+pref("extensions.anttoolbar.avconv", "/usr/bin/avconv");
+pref("extensions.anttoolbar.display_search_box", false);
+pref("extensions.anttoolbar.firstrun", false);
+pref("extensions.anttoolbar.flvdir", "~/Downloads/Ant Videos");
+pref("extensions.anttoolbar.show_not_found_window", false);
+
