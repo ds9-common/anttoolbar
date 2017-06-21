6204a3923ea3e7fb36790710495229fc164e9873
Rohan Garg <rohan@garg.io>
2017-06-21 17:53:42 +0200

    Fix path

--- debian/xul-ext-anttoolbar.js
+++ debian/xul-ext-anttoolbar.js
@@ -1,6 +1,6 @@
 pref("extensions.anttoolbar.avconv", "/usr/bin/avconv");
 pref("extensions.anttoolbar.display_search_box", false);
 pref("extensions.anttoolbar.firstrun", false);
-pref("extensions.anttoolbar.flvdir", "~/Downloads/Ant Videos");
+pref("extensions.anttoolbar.flvdir", "~/Videos");
 pref("extensions.anttoolbar.show_not_found_window", false);
 pref("extensions.anttoolbar.version", "@DEB_VERSION_UPSTREAM@");
